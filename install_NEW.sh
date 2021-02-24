5d270174ca47291e6e42719bc31fdf7d7a2b9ffa
kravenrus <47505285+kravenrus@users.noreply.github.com>
2021-02-25 00:30:48 +0400

    Update

--- install_NEW.sh
+++ install_NEW.sh
@@ -17,10 +17,9 @@ sudo a2ensite example.com.conf
 sudo a2dissite 000-default.conf
 sudo a2dissite default-ssl.conf
 mkdir -p /var/www/example.com/public_html
-cat index.html > /var/www/example.com/public_html/index.html
+# cat index.html > /var/www/example.com/public_html/index.html
 # cat index.php > /var/www/example.com/public_html/index.php
 chown -R www-data:www-data /var/www
-sudo service apache2 stop
 ####################################################################
 sudo apt install -y mariadb-server
 sudo service mysql start
@@ -47,19 +46,17 @@ sleep 5
 sudo service mysql stop
 cat phpmyadmin.conf > /etc/apache2/conf-available/phpmyadmin.conf
 sudo a2enconf phpmyadmin.conf
-
-sudo service apache2 start
-sudo service mysql start
+sudo service mysql restart
+sudo service apache2 restart
 ####################################################################
-#apt install -y gnupg2 net-tools
-#wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
-#echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
-#apt update
-#apt -y install postgresql-12 postgresql-client-12
-#################
-#ALTER USER postgres with PASSWORD 'postgres';
-#################
-#nano /etc/postgresql/12/main/postgresql.conf (listen_addresses = '*')
-#nano /etc/postgresql/12/main/pg_hba.conf (pg_hba.conf: host all all 0.0.0.0/0 md5)
-#systemctl restart postgresql
-#apt install -y pgadmin4 pgadmin4-apache2
+sudo apt install -y gnupg2 net-tools wget
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
+sudo apt update
+sudo apt -y install postgresql-12 postgresql-client-12
+sudo -u postgres psql -f postgres.sql
+cat postgresql.conf > /etc/postgresql/12/main/postgresql.conf
+cat pg_hba.conf > /etc/postgresql/12/main/pg_hba.conf
+sudo service postgresql restart
+####################################################################
+sudo apt install -y pgadmin4 pgadmin4-apache2
